e2f485ee1651cacd0978fd7aa7a408a309ff5b15
Chinmay Maheshwari <chinmay.m@browserstack.com>
2023-06-04 00:19:50 +0530

    Added logic for ignore Region

--- index.js
+++ index.js
@@ -46,6 +46,15 @@ module.exports.request = async function request(data) {
   await utils.postScreenshot(data);
 }; // To mock in test case
 
+module.exports.getElementIdFromElements = async function getElementIdFromElements(elements) {
+  const ignoredElementsArray = [];
+  for (let index = 0; index < elements.length; index++) {
+    const elementId = await elements[index].getId();
+    ignoredElementsArray.push(elementId);
+  }
+  return ignoredElementsArray;
+};
+
 module.exports.percyScreenshot = async function percyScreenshot(driver, name, options) {
   if (!driver || typeof driver === 'string') {
     // Unable to test this as couldnt define `browser` from test mjs file
@@ -86,15 +95,19 @@ module.exports.percyScreenshot = async function percyScreenshot(driver, name, op
       interceptor.restore();
     }
 
+    if (options && 'ignore_region_selenium_elements' in options) {
+      options.ignore_region_selenium_elements = await module.exports.getElementIdFromElements(options.ignore_region_selenium_elements);
+    }
+    console.log(options);
     // Post the driver details to the automate screenshot endpoint with snapshot options and other info
     await module.exports.request({
-      ...options,
       environmentInfo: ENV_INFO,
       clientInfo: CLIENT_INFO,
       sessionId: sessionId,
       commandExecutorUrl: commandExecutorUrl,
       capabilities: capabilities,
-      snapshotName: name
+      snapshotName: name,
+      options: options
     });
   } catch (error) {
     // Handle errors
