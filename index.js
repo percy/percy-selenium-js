ef00b982d5ca302d83fe933d5986f711d8220097
chinmay-browserstack <92926953+chinmay-browserstack@users.noreply.github.com>
2023-07-04 17:05:32 +0530

    Add POA Support (#333)
    * Dev complete for selenium webdriver
    
    * WDIO changes
    
    * Addressed comments
    
    * Remove extra file
    
    * Add lock file + refactor exports
    
    * Added comments
    
    * POA Support for ignoreRegion (#340)
    
    * Added logic for ignore Region
    
    * Added testcases
    
    * Remove extra console log
    
    * test Probable fix 1
    
    * Changes according to comment
    
    * Pplt 2059 fix 1 (#352)
    
    * Adding Code Scanner Semgrep.yml workflow file
    
    * Fixing function call name
    
    ---------
    
    Co-authored-by: bstack-security-github <116066275+bstack-security-github@users.noreply.github.com>
    
    * Resolving version dependencies
    
    ---------
    
    Co-authored-by: Amit Singh Sansoya <tusharamit@yahoo.com>
    Co-authored-by: bstack-security-github <116066275+bstack-security-github@users.noreply.github.com>

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const seleniumPkg = require('selenium-webdriver/package.json');
 const CLIENT_INFO = `${sdkPkg.name}/${sdkPkg.version}`;
 const ENV_INFO = `${seleniumPkg.name}/${seleniumPkg.version}`;
 const utils = require('@percy/sdk-utils');
+const { RequestInterceptor } = require('node-request-interceptor');
+const withDefaultInterceptors = require('node-request-interceptor/lib/presets/default');
 
 // Take a DOM snapshot and post it to the snapshot endpoint
 module.exports = async function percySnapshot(driver, name, options) {
@@ -37,5 +39,73 @@ module.exports = async function percySnapshot(driver, name, options) {
     // Handle errors
     log.error(`Could not take DOM snapshot "${name}"`);
     log.error(error);
+  }
+};
+
+module.exports.request = async function request(data) {
+  await utils.captureAutomateScreenshot(data);
+}; // To mock in test case
+
+const getElementIdFromElements = async function getElementIdFromElements(elements) {
+  return Promise.all(elements.map(e => e.getId()));
+};
+
+module.exports.percyScreenshot = async function percyScreenshot(driver, name, options) {
+  if (!driver || typeof driver === 'string') {
+    // Unable to test this as couldnt define `browser` from test mjs file
+    try {
+      // browser is defined in wdio context
+      // eslint-disable-next-line no-undef
+      [driver, name, options] = [browser, driver, name];
+    } catch (e) { // ReferenceError: browser is not defined.
+      driver = undefined;
+    }
+  }
+
+  if (!driver) throw new Error('An instance of the selenium driver object is required.');
+  if (!name) throw new Error('The `name` argument is required.');
+  if (!(await utils.isPercyEnabled())) return;
+  let log = utils.logger('selenium-webdriver');
+
+  try {
+    let sessionId, capabilities, commandExecutorUrl;
+    if (driver.constructor.name === 'Browser') { // Logic for wdio
+      sessionId = driver.sessionId;
+      capabilities = driver.capabilities;
+      commandExecutorUrl = `${driver.options.protocol}://${driver.options.hostname}${driver.options.path}`;
+    } else { // Logic for selenium-webdriver
+      const session = await driver.getSession();
+      sessionId = session.getId();
+      capabilities = Object.fromEntries(session.getCapabilities().map_);
+
+      // To intercept request from driver. used to get remote server url
+      const interceptor = new RequestInterceptor(withDefaultInterceptors.default);
+      interceptor.use((req) => {
+        const url = req.url.href;
+        commandExecutorUrl = url.split('/session')[0];
+      });
+      // making a call so we can intercept commandExecutorUrl
+      await driver.getCurrentUrl();
+      // To stop intercepting request
+      interceptor.restore();
+    }
+
+    if (options && 'ignore_region_selenium_elements' in options) {
+      options.ignore_region_selenium_elements = await getElementIdFromElements(options.ignore_region_selenium_elements);
+    }
+    // Post the driver details to the automate screenshot endpoint with snapshot options and other info
+    await module.exports.request({
+      environmentInfo: ENV_INFO,
+      clientInfo: CLIENT_INFO,
+      sessionId: sessionId,
+      commandExecutorUrl: commandExecutorUrl,
+      capabilities: capabilities,
+      snapshotName: name,
+      options: options
+    });
+  } catch (error) {
+    // Handle errors
+    log.error(`Could not take Screenshot "${name}"`);
+    log.error(error.stack);
   }
 };
--- test/index.test.mjs
+++ test/index.test.mjs
@@ -1,6 +1,7 @@
 import webdriver from 'selenium-webdriver';
 import helpers from '@percy/sdk-utils/test/helpers';
 import percySnapshot from '../index.js';
+const { percyScreenshot } = percySnapshot;
 
 describe('percySnapshot', () => {
   let driver;
@@ -59,6 +60,91 @@ describe('percySnapshot', () => {
 
     expect(helpers.logger.stderr).toEqual(jasmine.arrayContaining([
       '[percy] Could not take DOM snapshot "Snapshot 1"'
+    ]));
+  });
+});
+
+describe('percyScreenshot', () => {
+  class Browser { // Mocking WDIO driver
+    constructor() {
+      this.sessionId = '123';
+      this.capabilities = { browserName: 'chrome' };
+      this.options = { protocol: 'https', path: '/wd/hub', hostname: 'hub-cloud.browserstack.com' };
+    }
+  }
+  let driver;
+
+  beforeAll(async function() {
+    driver = {
+      getSession: () => {
+        return new Promise((resolve, _) => resolve({
+          getId: () => { return '123'; },
+          getCapabilities: () => { return { map_: new Map([['browserName', 'chrome'], ['platform', 'WINDOWS'], ['version', '123']]) }; }
+        }));
+      },
+      getCurrentUrl: () => { return new Promise((resolve, _) => resolve(helpers.mockGetCurrentUrl())); }
+    };
+  });
+
+  beforeEach(async () => {
+    await helpers.setupTest();
+  });
+
+  it('throws an error when a driver is not provided', async () => {
+    await expectAsync(percyScreenshot())
+      .toBeRejectedWithError('An instance of the selenium driver object is required.');
+  });
+
+  it('throws an error when a name is not provided', async () => {
+    await expectAsync(percyScreenshot(driver))
+      .toBeRejectedWithError('The `name` argument is required.');
+  });
+
+  it('disables snapshots when the healthcheck fails', async () => {
+    await helpers.test('error', '/percy/healthcheck');
+
+    await percyScreenshot(driver, 'Snapshot 1');
+    await percyScreenshot(driver, 'Snapshot 2');
+
+    expect(helpers.logger.stdout).toEqual(jasmine.arrayContaining([
+      '[percy] Percy is not running, disabling snapshots'
+    ]));
+  });
+
+  it('posts driver details to the local percy server', async () => {
+    const mockedPostCall = spyOn(percySnapshot, 'request').and.callFake(() => {});
+    await percyScreenshot(driver, 'Snapshot 1');
+    expect(mockedPostCall).toHaveBeenCalledWith(jasmine.objectContaining({
+      sessionId: '123', commandExecutorUrl: 'http://localhost:5338/wd/hub', snapshotName: 'Snapshot 1'
+    }));
+  });
+
+  it('posts driver details to the local percy server with ignore region', async () => {
+    const element = { getId: () => {} };
+    const mockElement = spyOn(element, 'getId').and.callFake(() => { return new Promise((resolve, _) => resolve('123')); });
+    const mockedPostCall = spyOn(percySnapshot, 'request').and.callFake(() => {});
+    await percyScreenshot(driver, 'Snapshot 2', { ignore_region_selenium_elements: [element] });
+    expect(mockElement).toHaveBeenCalled();
+    expect(mockedPostCall).toHaveBeenCalledWith(jasmine.objectContaining({
+      sessionId: '123', commandExecutorUrl: 'http://localhost:5338/wd/hub', snapshotName: 'Snapshot 2', options: { ignore_region_selenium_elements: ['123'] }
+    }));
+  });
+
+  it('posts driver details to the local percy server with wdio', async () => {
+    driver = new Browser();
+    const mockedPostCall = spyOn(percySnapshot, 'request').and.callFake(() => {});
+    await percyScreenshot(driver, 'Snapshot 1', {});
+    expect(mockedPostCall).toHaveBeenCalledWith(jasmine.objectContaining({
+      sessionId: '123', commandExecutorUrl: 'https://hub-cloud.browserstack.com/wd/hub', snapshotName: 'Snapshot 1'
+    }));
+  });
+
+  it('handles snapshot failures', async () => {
+    await helpers.test('error', '/percy/automateScreenshot');
+    await percyScreenshot(driver, 'Snapshot 1');
+
+    expect(helpers.logger.stderr).toEqual(jasmine.arrayContaining([
+      '[percy] Could not take Screenshot "Snapshot 1"'
     ]));
   });
 });
