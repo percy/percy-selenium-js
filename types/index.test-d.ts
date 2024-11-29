14d69d483b1ed4823189b3f8e72f390bbbe1701c
Ninad Sheth <ninad@browserstack.com>
2024-11-29 17:01:19 +0530

    Adds support for new type params

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -5,15 +5,15 @@ let driver = {}
 expectError(percySnapshot());
 expectError(percySnapshot('Snapshot name'));
 
-expectType<Promise<void>>(percySnapshot(driver, 'Snapshot name'));
-expectType<Promise<void>>(percySnapshot(driver, 'Snapshot name', { widths: [1000] }));
+expectType<Promise<void | { [key: string]: any }>>(percySnapshot(driver, 'Snapshot name'));
+expectType<Promise<void | { [key: string]: any }>>(percySnapshot(driver, 'Snapshot name', { widths: [1000] }));
 
 expectError(percySnapshot(driver, 'Snapshot name', { foo: 'bar' }));
 
 expectError(percyScreenshot());
 expectError(percyScreenshot('Snapshot name'));
 
-expectType<Promise<void>>(percyScreenshot(driver, 'Snapshot name'));
-expectType<Promise<void>>(percyScreenshot(driver, 'Snapshot name', { widths: [1000] }));
+expectType<Promise<void | { [key: string]: any }>>(percyScreenshot(driver, 'Snapshot name'));
+expectType<Promise<void | { [key: string]: any }>>(percyScreenshot(driver, 'Snapshot name', { widths: [1000] }));
 
-expectType<Promise<void>>(percyScreenshot(driver, 'Snapshot name', { foo: 'bar' }));
+expectType<Promise<void | { [key: string]: any }>>(percyScreenshot(driver, 'Snapshot name', { foo: 'bar' }));
