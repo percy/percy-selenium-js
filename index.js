231fe9953c1d64760c7d1ca5de32c8ec473d7cf5
amandeepsingh333 <amandeep@browserstack.com>
2025-03-28 09:58:44 +0530

    added regions

--- index.js
+++ index.js
@@ -144,6 +144,58 @@ async function currentURL(driver, options) {
   return url;
 }
 
+const createRegion = function({
+  boundingBox = null,
+  elementXpath = null,
+  elementCSS = null,
+  padding = null,
+  algorithm = 'ignore',
+  diffSensitivity = null,
+  imageIgnoreThreshold = null,
+  carouselsEnabled = null,
+  bannersEnabled = null,
+  adsEnabled = null,
+  diffIgnoreThreshold = null
+} = {}) {
+  const elementSelector = {};
+  if (boundingBox) elementSelector.boundingBox = boundingBox;
+  if (elementXpath) elementSelector.elementXpath = elementXpath;
+  if (elementCSS) elementSelector.elementCSS = elementCSS;
+
+  const region = {
+    algorithm,
+    elementSelector
+  };
+
+  if (padding) {
+    region.padding = padding;
+  }
+
+  const configuration = {};
+  if (['standard', 'intelliignore'].includes(algorithm)) {
+    if (diffSensitivity !== null) configuration.diffSensitivity = diffSensitivity;
+    if (imageIgnoreThreshold !== null) configuration.imageIgnoreThreshold = imageIgnoreThreshold;
+    if (carouselsEnabled !== null) configuration.carouselsEnabled = carouselsEnabled;
+    if (bannersEnabled !== null) configuration.bannersEnabled = bannersEnabled;
+    if (adsEnabled !== null) configuration.adsEnabled = adsEnabled;
+  }
+
+  if (Object.keys(configuration).length > 0) {
+    region.configuration = configuration;
+  }
+
+  const assertion = {};
+  if (diffIgnoreThreshold !== null) {
+    assertion.diffIgnoreThreshold = diffIgnoreThreshold;
+  }
+
+  if (Object.keys(assertion).length > 0) {
+    region.assertion = assertion;
+  }
+
+  return region;
+};
+
 // Take a DOM snapshot and post it to the snapshot endpoint
 const percySnapshot = async function percySnapshot(driver, name, options) {
   if (!driver) throw new Error('An instance of the selenium driver object is required.');
@@ -188,6 +240,7 @@ const percySnapshot = async function percySnapshot(driver, name, options) {
 
 module.exports = percySnapshot;
 module.exports.percySnapshot = percySnapshot;
+module.exports.createRegion = createRegion;
 
 module.exports.request = async function request(data) {
   return await utils.captureAutomateScreenshot(data);
